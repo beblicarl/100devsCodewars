8fe45eb5b41c64da8e02f5244b662378392b012d
Carl Bebli <beblicarl.cb@gmail.com>
2022-05-24 18:18:22 +0000

    codewars

---
+++ 24th-May/sayHello.js
@@ -0,0 +1,18 @@
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+//pseudo code
+//You add the string "Hello," at the beginning of value passed into name parameter
+
+solution;
+function sayHello(name) {
+  return "Hello, " + name;
+}
+
+function sayHello(name) {
+  return "Hello," + " " + name;
+}
