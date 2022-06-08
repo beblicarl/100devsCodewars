27b03b19a83498944f7a096ad288b368797d3b64
Carl Bebli <beblicarl.cb@gmail.com>
2022-06-08 19:03:55 +0000

    codewars

---
+++ 8th-June/switchItUp.js
@@ -0,0 +1,12 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+solution;
+
+link;
+// https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/javascript
