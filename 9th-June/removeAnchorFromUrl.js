c551dd7043948aae4ebf9d946478d0f096f5c565
Carl Bebli <beblicarl.cb@gmail.com>
2022-06-09 17:48:31 +0000

    codewars

---
+++ 9th-June/removeAnchorFromUrl.js
@@ -0,0 +1,20 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+solution;
+
+//pseudo code
+//split the string by the #
+//return the 0th index item from the array
+
+function removeUrlAnchor(url){
+    return url.split("#")[0]
+  }
+
+
+//test cases
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
