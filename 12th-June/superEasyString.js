63ed384a755dc37b422c2465d1977f024cb63f3c
Carl Bebli <beblicarl.cb@gmail.com>
2022-06-12 18:02:13 +0000

    codewars

---
+++ 12th-June/superEasyString.js
@@ -0,0 +1,17 @@
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+solution;
+
+//pseudo code
+//you check if x is a string
+//if x is a string you return error
+//if x is not a string you return ( x * 50) + 6
+
+function problem(x) {
+  return x === x.toString() ? "Error" : x * 50 + 6;
+}
+
+//test cases
+problem("hello");
+problem(5);
+problem("heya");
