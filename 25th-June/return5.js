79b7ef28dda6d863479c545113f30fdf7c606269
Carl Bebli <beblicarl.cb@gmail.com>
2022-06-25 13:06:37 +0000

    codewars

---
+++ 25th-June/return5.js
@@ -0,0 +1,28 @@
+/*
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+
+Parameters
+None
+
+Result
+Number
+
+Example
+return 5
+
+Pseudocode
+1. Store a string of length 5  in a variable and return the length
+
+
+
+*/
+
+solution;
+function unusualFive() {
+  let string = "world";
+  return string.length;
+}
