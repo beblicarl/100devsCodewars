5c05d8b8f8a0d0efecdb7767521832178e3d5b06
Carl Bebli <beblicarl.cb@gmail.com>
2022-06-29 13:20:17 +0000

    codewars

--- 29th-June/mathematicalOperations.js
+++ 29th-June/mathematicalOperations.js
@@ -13,7 +13,7 @@ basicOp('/', 49, 7), 7);
 
 Pseudocode
 1. Check the values of the operations
-2. If + add value1 and value 2
+2. If "+" add value1 and value 2
 2. If - subtract value1 and value2
 4. If * multiply value1 and value2
 5. If /  divide value1 and value2
