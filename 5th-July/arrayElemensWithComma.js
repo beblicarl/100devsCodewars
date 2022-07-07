6ee094a637589746d61111652e2d3079a73842a4
Carl Bebli <beblicarl.cb@gmail.com>
2022-07-07 18:46:47 +0000

    codewars

--- 5th-July/arrayElemensWithComma.js
+++ 5th-July/arrayElemensWithComma.js
@@ -1,4 +1,15 @@
 /*
 
-https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
 */
+
+solution;
+function printArray(array) {
+  return array.join(",");
+}
