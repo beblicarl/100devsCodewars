142176dbff8196bc7f36bc7900b3a945299b66c7
Carl Bebli <beblicarl.cb@gmail.com>
2022-07-19 15:14:45 +0000

    Codewars

--- 17th-July/trainingJs.js
+++ 17th-July/trainingJs.js
@@ -1,4 +1,55 @@
 /*
 
-https://www.codewars.com/kata/571edd157e8954bab500032d/train/javascript
+In javascript, Number is one of basic data types. It can be positive: 1,2,3, negative:-1,-100 , integer:123,456, decimal:3.1415926,-8.88 etc..
+
+Numbers can use operators such as + - * / %
+
+Task
+I've written five function equal1,equal2,equal3,equal4,equal5, defines six global variables v1 v2 v3 v4 v5 v6, every function has two local variables a,b, please set the appropriate value for the two variables(select from v1--v6), making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions
+
+Parameters
+Numbers
+
+Result
+Numbers
+
+Example
+(equal1(),100,"value of a+b is not equal to 100");
+equal2(),100,"value of a-b is not equal to 100");
+equal3(),100,"value of a*b is not equal to 100");
+equal4(),100,"value of a/b is not equal to 100");
+equal5(),100,"value of a%b is not equal to 100");
+
+Pseudocode
+1. Keeping the operator in mind, make sure every two variables operation returned will result to 100
 */
+
+solution;
+var v1 = 50;
+(v2 = 100), (v3 = 150), (v4 = 200), (v5 = 2), (v6 = 250);
+function equal1() {
+  var a = v1;
+  var b = v1;
+  return a + b;
+}
+//Please refer to the example above to complete the following functions
+function equal2() {
+  var a = v4; //set number value to a
+  var b = v2; //set number value to b
+  return a - b;
+}
+function equal3() {
+  var a = v1; //set number value to a
+  var b = v5; //set number value to b
+  return a * b;
+}
+function equal4() {
+  var a = v4; //set number value to a
+  var b = v5; //set number value to b
+  return a / b;
+}
+function equal5() {
+  var a = v6; //set number value to a
+  var b = v3; //set number value to b
+  return a % b;
+}
