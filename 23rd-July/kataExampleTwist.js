f6d121ebc3cad629d3b9fa41ffc2cba2a5e67827
Carl Bebli <beblicarl.cb@gmail.com>
2022-07-26 14:38:46 +0000

    codewars

--- 23rd-July/kataExampleTwist.js
+++ 23rd-July/kataExampleTwist.js
@@ -1,4 +1,21 @@
 /*
-https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/javascript
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+
+Parameter
+String
+
+Result
+Array of strings
+
+Pseudocode
+1. Iterate 1000 times and push codewars to the empty array
 
 */
+solution;
+// add the value "codewars" to the websites array 1,000 times
+let websites = [];
+for (let i = 0; i < 1000; i++) {
+  websites.push("codewars");
+}
