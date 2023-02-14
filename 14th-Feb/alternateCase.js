d31fa498172ff30b942e9e14cd9e5fa678bd732c
Carl Bebli <beblicarl.cb@gmail.com>
2023-02-14 09:59:33 +0000

    14th February codewars

---
+++ 14th-Feb/alternateCase.js
@@ -0,0 +1,36 @@
+/*
+
+altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+Parameter
+String
+
+Result
+String
+
+Example
+"hello world".toAlternatingCase() === "HELLO WORLD"
+"HELLO WORLD".toAlternatingCase() === "hello world"
+"hello WORLD".toAlternatingCase() === "HELLO world"
+"HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+"12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
+"1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
+"String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+As usual, your function/method should be pure, i.e. it should not mutate the original string.
+
+Pseudocode
+1. Split the string
+2. Map through the array of string and check if the element is equal to the capitalized form
+3. If yes, return the lower case form
+4. Return the join result
+
+
+
+*/
+
+solution
+String.prototype.toAlternatingCase = function () {
+    return this.split("").map(a => a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()).join('')
+  }
+
